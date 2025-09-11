68e1a1afa88de4e6a36e3d4cd82336297890f1bf
Aashish Kumar Singh <aashishsinghjsr81@gmail.com>
2025-09-11 09:55:20 +0530

    Designed a System for Zomato food delivery app

---
+++ Zomato/OrderFactory.java
@@ -0,0 +1,8 @@
+package Zomato;
+
+import java.util.List;
+
+public interface OrderFactory {
+    Order createOrder(User user, Cart cart, Restaurant restaurant, List<MenuItem> menuItems,
+            PaymentStrategy paymentStrategy, double totalCost, String orderType);
+}
